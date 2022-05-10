6bfde536c03ca5253ef9ca810ecb7f77bba39af7
meinhoonharsh <harshprogrammer782@gmail.com>
2022-05-11 00:24:55 +0530

    Added fields in USers Migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -30,6 +30,8 @@ return new class extends Migration
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
+            $table->string('cover_photo_path', 2048)->nullable();
+            $table->boolean('active')->default(true);
             $table->timestamps();
         });
     }
