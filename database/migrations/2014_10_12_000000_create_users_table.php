8efec4fae2b1cc07ebbc5c7cddd130bc65506fc3
meinhoonharsh <harshprogrammer782@gmail.com>
2022-05-10 23:41:52 +0530

    Added fields in create_user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,13 +16,17 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')->unique();$table->string('contact')->nullable();
+            $table->date('dob')->nullable();
+            $table->string('college')->nullable();
+            $table->string('graduation_year')->nullable();
+            $table->string('degree')->nullable();
+            $table->string('department')->nullable();
+            $table->json('images')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->integer('role')->default(1);
             $table->string('google_id')->nullable();
-            $table->string('linkedin_id')->nullable();
-            $table->string('github_id')->nullable();
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
