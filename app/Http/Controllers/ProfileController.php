20c823fa44840fcadb89e38eae0b398123b3eb19
meinhoonharsh <harshprogrammer782@gmail.com>
2022-05-16 13:02:21 +0530

    Created Get profile Function

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Auth;
+
 
 class ProfileController extends Controller
 {
     //
+    public function getProfile()
+    {
+        $user = Auth::user();
+        return $this->sendResponse($user, 'User profile retrieved successfully.');
+    }
 }
