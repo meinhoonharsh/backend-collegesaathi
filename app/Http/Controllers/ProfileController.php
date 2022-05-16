8e6788487c4f49eb8010878825104891b5e60d6d
meinhoonharsh <harshprogrammer782@gmail.com>
2022-05-16 13:12:04 +0530

    Created Update Profile Function

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -15,4 +15,19 @@ class ProfileController extends Controller
         $user = Auth::user();
         return $this->sendResponse($user, 'User profile retrieved successfully.');
     }
+
+    public function updateProfile(Request $request)
+    {
+        $user = Auth::user();
+        $user->name = $request->name;
+        $user->contact = $request->contact;
+        $user->dob = $request->dob;
+        $user->college = $request->college;
+        $user->graduation_year = $request->graduation_year;
+        $user->degree = $request->degree;
+        $user->department = $request->department;
+        $user->images = $request->images;
+        $user->save();
+        return $this->sendResponse($user, 'User profile updated successfully.');
+    }
 }
